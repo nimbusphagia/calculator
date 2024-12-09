b24833ed7f776bcf2d69898617470f55d8ccb3cb
Ignacio <185011694+nimbusphagia@users.noreply.github.com>
2024-12-08 20:42:05 -0500

    FIXED  RESULT, BACKSPACE, AND BEGINNING BUGS

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ function operate(num1, op, num2){
             result = root(a, b);
             break;    
     }
-    return result;
+    return Math.ceil(result * 100000000) / 100000000;
 }
 //INPUT AND OUTPUT FUNCTION
 const btnAll = Array.from(document.querySelectorAll(".button"));
@@ -71,11 +71,17 @@ const inputScreen = document.querySelector("#input");
 let input = "";
 
 function displayInput(event){
-    input = event.target.textContent;
-    inputScreen.textContent += input.replace("=", "").replace("n", "").replace("AC", "").replace("⌫", "");
+    if (inputScreen.textContent === "..."){
+        input = event.target.textContent;
+        inputScreen.textContent += input.replace("=", "").replace("n", "").replace("AC", "").replace("⌫", "").replace(/\D/g, "");
+    }else{
+        input = event.target.textContent;
+        inputScreen.textContent += input.replace("=", "").replace("n", "").replace("AC", "").replace("⌫", "");
+    }
+    
 }
 function getTemp(event){
-    temp += event.target.textContent.replace("n", "").replace("AC", "").replace("⌫", "");;
+    temp += event.target.textContent.replace("AC", "").replace("⌫", "");
 }
 
 for(let i = 0; i < btnAll.length; i++){
@@ -91,26 +97,33 @@ let newDisplay;
  
 
 function clearAll(){
-    inputScreen.textContent = "";
+    inputScreen.textContent = "...";
     temp = "";
     operand1 = "";
     operand2 = "";
     operator = "";
     outputScreen.textContent = "output";
 }
+
 function backtrack(){
-    lastChar = (inputScreen.textContent.length) - 1;
-    newDisplay = inputScreen.textContent.substring(0, lastChar);
-    inputScreen.textContent = newDisplay;
-    if(num2){
-    lastChar = (num2.length) - 1;
-    newDisplay = num2.substring(0, lastChar);
-    num2 = newDisplay;
-    } else if (temp){
-    lastChar = (temp.length) - 1;
-    newDisplay = temp.substring(0, lastChar);
-    temp = newDisplay;
-    }
+    if (proof === "=" || inputScreen.textContent === "..."){
+        outputScreen.textContent = "(｡•́︿•̀｡)";
+    } else if (!temp){
+        lastChar = (inputScreen.textContent.length) - 1;
+        newDisplay = inputScreen.textContent.substring(0, lastChar);
+        inputScreen.textContent = newDisplay;
+        deleted = num1;
+        operator = "";
+        console.log(deleted);           
+        } else if(temp){
+        lastChar = (inputScreen.textContent.length) - 1;
+        newDisplay = inputScreen.textContent.substring(0, lastChar);
+        inputScreen.textContent = newDisplay;
+        lastChar = (temp.length) - 1;
+        newDisplay = temp.substring(0, lastChar);
+        temp = newDisplay;
+        console.log(temp);
+        }
 }
 
 btnAC.addEventListener("click", clearAll);
@@ -118,20 +131,26 @@ btnBacktrack.addEventListener("click", backtrack);
 
 //NEW APPROACH
 const btnContainer = document.querySelector("#btnContainer");
-
-
 let num1 = "";
 let operator = "";
 let num2 = "";
 let result = "";
 let temp = "";
+let deleted = "";
+let proof = "";
 
 function calculate (event){
     //!operator ? startMath() : continueMath();
-    if (!operator){
-        operator = event.target.textContent;
-        num1 = temp.replace(operator, "");
+    if (inputScreen.textContent === "..."){
         temp = "";
+        inputScreen.textContent = "...";
+        proof = "";
+    } else if(!operator){
+        operator = event.target.textContent;
+        num1 = deleted ? deleted : temp.replace(operator, "");
+        temp = "";
+        deleted= "";
+        proof = "";
     }else if (event.target.textContent === "="){
             num2 = temp.replace(event.target.textContent, "");
             result = operate(num1, operator, num2);
@@ -140,6 +159,7 @@ function calculate (event){
             num2 = "";
             temp = result;
             outputScreen.textContent = result;
+            proof = "=";
         } else {
             num2 = temp.replace(event.target.textContent, "");
             result = operate(num1, operator, num2);
@@ -148,6 +168,7 @@ function calculate (event){
             num2 = "";
             temp = "";
             outputScreen.textContent = result;
+            proof = "";
         }
 }
 
